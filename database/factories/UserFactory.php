af11f7debfa8ac6fbf1562a3d8eb5370e05a2801
Danton de Lima <dantondelima.contato@gmail.com>
2025-04-25 02:03:54 -0300

    feat(UserState): add state machine for user status

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,39 +4,31 @@ declare(strict_types=1);
 
 namespace Database\Factories;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Facades\Hash;
+use App\Enums\UserRoleEnum;
 use Illuminate\Support\Str;
+use App\States\User\Created;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
- */
 final class UserFactory extends Factory
 {
-    /**
-     * The current password being used by the factory.
-     */
     protected static ?string $password;
 
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
+        $role = fake()->randomElement(UserRoleEnum::cases());
+
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => self::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'role' => $role->value,
+            'status' => Created::$name,
         ];
     }
 
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
     public function unverified(): static
     {
         return $this->state(fn (array $attributes) => [
