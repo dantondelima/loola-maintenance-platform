4f28958ab007b3c44b1faddab5c2cf9088cd52da
Danton <dantondelima.contato@gmail.com>
2025-04-26 02:08:53 -0300

    chore: add dockerfile and configurations for local/prod environment

---
+++ docker/app/docker-entrypoint.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+set -e
+
+echo "🚀 Starting container in environment: ${APP_ENV:-production}"
+
+if [ ! -f "./artisan" ]; then
+    echo "❌ Invalid Laravel project directory: $PWD"
+    exit 1
+fi
+
+# If local environment, prepare
+if [ "$APP_ENV" = "local" ]; then
+    if [ ! -f "./vendor/autoload.php" ]; then
+        echo "📦 Running composer install (local development)..."
+        composer install
+    fi
+
+    echo "🔑 Running artisan key:generate (local development)..."
+    php artisan key:generate
+
+    echo "🎯 Running database migrations and seeds (local development)..."
+    php artisan migrate --seed
+else
+    # In production environment
+    echo "Caching Laravel config and routes (production)..."
+    php artisan config:cache
+    php artisan route:cache
+fi
+
+exec "$@"
