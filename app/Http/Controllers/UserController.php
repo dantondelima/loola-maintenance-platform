fca1b73c6259e448270bc03108a3718312a7776c
Danton de Lima <dantondelima.contato@gmail.com>
2025-05-01 19:58:58 -0300

    feat(UserController): add /me to user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,12 +4,11 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
-use App\Dtos\User\UserStoreDto;
-use App\Http\Resources\UserResource;
-use App\Services\CreateUserService;
-use Exception;
-use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use App\Dtos\User\UserStoreDto;
+use Illuminate\Http\JsonResponse;
+use App\Http\Resources\UserResource;
+use App\Services\User\CreateUserService;
 
 final class UserController extends BaseController
 {
@@ -18,25 +17,26 @@ final class UserController extends BaseController
         try {
             $user = $service->handle($data);
 
-            return $this->respondCreated(new UserResource($user));
-        } catch (Exception $e) {
+            return $this->respondCreated((new UserResource($user))->withToken());
+        } catch (\Throwable $e) {
             report($e);
 
             return $this->respondWithError();
         }
     }
 
-    public function show(string $id)
+    public function me()
     {
-        //
+        try {
+            return $this->respondWithSuccess((new UserResource(auth()->user())));
+        } catch (\Throwable $e) {
+            report($e);
+
+            return $this->respondWithError();
+        }
     }
 
     public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    public function destroy(string $id)
     {
         //
     }
--- app/Services/User/CreateUserService.php
+++ app/Services/User/CreateUserService.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Services;
+namespace App\Services\User;
 
 use App\Dtos\User\UserStoreDto;
 use App\Models\User;
